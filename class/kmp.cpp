3f7c6e0e10da5d4794807abcf3bdc6cd1c19a0c7
Mario Frias <a01782559@tec.mx>
2023-09-07 10:22:27 -0600

    kmp finished

--- class/kmp.cpp
+++ class/kmp.cpp
@@ -2,11 +2,11 @@
 #include <cstdlib>
 #include <cstring>
 
-int *prefix_function(char *pattern)
+int     *prefix_function(char *pattern)
 {
     int x = 0, y = 1;
     int *fun = (int *)malloc(sizeof(int) * strlen(pattern));
-    fun[0] = -1;
+    fun[0] = 0;
 
     while (pattern[y] != '\0')
     {
@@ -23,7 +23,7 @@ int *prefix_function(char *pattern)
         }
         else
         {
-            fun[y] = -1;
+            fun[y] = 0;
             x = 0;
         }
         y++;
@@ -31,7 +31,31 @@ int *prefix_function(char *pattern)
     return(fun);
 }
 
-int main(int argc, char **argv)
+int    find_substring(char *main, char *pattern, int *match)
+{
+    int i, j = 0;
+
+    for (i=0; i < strlen(main); i++)
+    {
+        //printf("%c %c\n", main[i], pattern[j]);
+        if (pattern[j] == main[i])
+        {
+            j++;
+        }
+        else
+        {
+            //printf("Go to: %c in position %d\n", pattern[match[j -1]], match[j-1]);
+            j = match[j+1];
+        }
+        if (pattern[j] == '\0')
+        {
+            return (i + 1 - strlen(pattern));
+        }
+    }
+    return -1;
+}
+
+int     main(int argc, char **argv)
 {
     char *pattern = argv[1];
 
@@ -41,6 +65,9 @@ int main(int argc, char **argv)
         printf("%d ", fun[i]);
 
     printf("\n");
+
+    int substring_position = find_substring(argv[2], pattern, fun);
+    printf("Found the pattern '%s' in the main string '%s' starts in position %d '%s'\n", argv[1], argv[2], substring_position, &argv[2][substring_position]);
     free(fun);
     return 0;
 }
