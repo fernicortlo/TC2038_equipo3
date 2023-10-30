35723b289dc43b6039325d78c60733aeadd5c597
fernicortlo <a01026613@tec.mx>
2023-10-30 14:48:20 -0600

    agrega tercera parte

--- Integradora/main.hpp
+++ Integradora/main.hpp
@@ -7,3 +7,11 @@
 
 int z_function(const char *main, const char *pattern);
 int manacher(const char *main, int *biggest);
+
+struct LCS_Result {
+    int length;      // Length of the longest common substring
+    int start_index; // Starting index of the LCS in the first string
+    int end_index;   // Ending index of the LCS in the first string
+};
+LCS_Result longest_common_substring(const std::string& s1, const std::string& s2);
+
